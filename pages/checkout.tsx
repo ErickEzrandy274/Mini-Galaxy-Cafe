325f227528925d2bcbe5809c9e9392c5ed61fc59
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-04 13:45:11 +0700

    create basecode for checkout page

---
+++ pages/checkout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CheckOut from '../components/modules/CheckOut.tsx/CheckOut'
+import MainLayout from '../components/modules/MainLayout/MainLayout'
+
+const checkout = () => {
+    return (
+        <MainLayout>
+            <CheckOut />
+        </MainLayout>
+    )
+}
+
+export default checkout
