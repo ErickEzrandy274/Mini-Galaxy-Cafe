99e0e02c11a9885c25a230ce5f7f961457bebaed
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-23 19:59:30 +0700

    create checkDisabilityButton

--- components/utils/function/function.ts
+++ components/utils/function/function.ts
@@ -1,4 +1,9 @@
 import axios from "axios";
+import { LoginInputType, TypeForm } from "../../modules/Authentication/interface";
+
+interface DisabilityButtonParamType extends TypeForm, LoginInputType {
+	nickname?: string
+}
 
 let typeX = "";
 
@@ -32,3 +37,33 @@ export const extractError = (err: any) => {
 export const toCapitalize = (text: string) => {
 	return text.substring(0, 1).toUpperCase() + text.substring(1);
 };
+
+export const checkDisabilityButton = (item: DisabilityButtonParamType) => {
+	const feedback: { disable: boolean, message: string } = {
+		disable: false,
+		message: ""
+	}
+	// Taken from https://stackoverflow.com/questions/19605150/passwordRegex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+	// Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+	const passwordRegex: RegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+	// Taken from https://www.w3resource.com/javascript/form/email-validation.php
+	const emailRegex: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+	const nicknameRegex: RegExp = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
+
+	const { typeForm, email, password } = item
+	if (!passwordRegex.test(password)) {
+		return {...feedback, disable: true, message: "Password minimal mengandung delapan karakter, setidaknya satu huruf besar, satu huruf kecil, satu angka, dan satu karakter khusus!"}
+	} 
+
+	if (!emailRegex.test(email) {
+		return {...feedback, disable: true, message: "Email Anda tidak valid!"}
+	}
+
+	if (typeForm === "Register") {
+		if (item.nickname !== undefined && !nicknameRegex.test(item.nickname)) {
+			return {...feedback, disable: true, message: "Nickname Anda tidak valid!"}
+		}
+	}
+	
+}
