16fa84c4b84cb13139a5f3236a3f7fdcb20d6593
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-25 12:45:20 +0700

    create get data function to fetch all data

---
+++ components/utils/function/dataManipulation.ts
@@ -0,0 +1,34 @@
+import { collection, getDocs } from "firebase/firestore";
+import React, { SetStateAction } from "react";
+import { database, imageRef } from "../firebase/firebase";
+
+type DataFetchType = {
+	type: "Foods" | "Beverages" | "Snacks";
+	setData: React.Dispatch<SetStateAction<any[]>>;
+	setIsLoading: React.Dispatch<SetStateAction<boolean>>;
+};
+
+export const getData = async (item: DataFetchType) => {
+	const { type, setData, setIsLoading } = item;
+	setIsLoading(true)
+	const tempData: any[] = [];
+
+	const querySnapshot = await getDocs(collection(database, type));
+	querySnapshot.forEach(async (doc) => {
+		const { name, price, image, buyer } = doc.data();
+		const img = await imageRef(type, image._key.path.segments[8]);
+		const newData = {
+			dataId: doc.id,
+			name,
+			price,
+			image: img,
+			buyer,
+		};
+		tempData.push(newData);
+	});
+
+	setData(tempData);
+	setTimeout(() => {
+		setIsLoading(false);
+	}, 2000);
+};
