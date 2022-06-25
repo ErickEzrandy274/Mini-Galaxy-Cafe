bbf29df220f48874cd482e59b0620e27b82a1c27
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-25 12:39:10 +0700

    create image reference function to get download url

--- components/utils/firebase/firebase.ts
+++ components/utils/firebase/firebase.ts
@@ -1,19 +1,32 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArAh2Ivvtpgg1ztMqa8mDZZG2Z6QCV1NA",
-  authDomain: "mini-galaxy-cafe.firebaseapp.com",
-  projectId: "mini-galaxy-cafe",
-  storageBucket: "mini-galaxy-cafe.appspot.com",
-  messagingSenderId: "413795619439",
-  appId: "1:413795619439:web:5f508e247f9631015bb39a",
-  measurementId: "G-6PDKX5WH6K"
+	apiKey: "AIzaSyArAh2Ivvtpgg1ztMqa8mDZZG2Z6QCV1NA",
+	authDomain: "mini-galaxy-cafe.firebaseapp.com",
+	projectId: "mini-galaxy-cafe",
+	storageBucket: "mini-galaxy-cafe.appspot.com",
+	messagingSenderId: "413795619439",
+	appId: "1:413795619439:web:5f508e247f9631015bb39a",
+	measurementId: "G-6PDKX5WH6K",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+export const imageRef = async (
+	type: "Foods" | "Beverages" | "Snacks",
+	img: string
+) => {
+	try {
+		return await getDownloadURL(ref(storage, `${type}/${img}`));
+	} catch (err) {
+		console.log(err);
+	}
+};
 export const database = getFirestore(app);
 
-export const auth = getAuth(app)
+export const auth = getAuth(app);
