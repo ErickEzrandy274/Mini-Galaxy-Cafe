9520c7b30410cc31e20e43085219c5f41e396852
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-25 13:24:30 +0700

    migrate from django into firestore for beverage

--- pages/beverage.tsx
+++ pages/beverage.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loader from "../components/elements/Loader/Loader";
 import ProductPage from "../components/modules/ProductPage/ProductPage";
-import { getData, setType } from "../components/utils/function/function";
-import { dataProps } from "./api/hello";
+import { getData } from "../components/utils/function/dataManipulation";
 
-setType("beverage");
+const beverage = () => {
+	const [data, setData] = useState<any[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 
-const beverage: React.FC<dataProps> = ({ data }) => {
-	return <ProductPage data={data} type="Beverages" />;
+	useEffect(() => {
+		getData({ type: "Beverages", setData, setIsLoading });
+	}, []);
+
+	return isLoading ? <Loader /> : <ProductPage data={data} type="Beverages" />;
 };
 
-export const getServerSideProps = getData;
 
 export default beverage;
