6041689a90aef317f0ba87da66237e980b58c189
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-25 14:04:53 +0700

    remove unused domain

--- next.config.js
+++ next.config.js
@@ -3,18 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	images: {
 		domains: [
-			"harianresep.com","cdn1-production-images-kly.akamaized.net","awsimages.detik.net.id",
-      "d12man5gwydfvl.cloudfront.net","topwisata.info","harianresep.com","img.kurio.network",
-      "resepkoki.co","www.unileverfoodsolutions.co.id","cdn-cas.orami.co.id","rasabunda.com",
-      "cdn.yummy.co.id","www.resepistimewa.com","i0.wp.com","sweetrip.id","cdn-2.tstatic.net",
-      "assets.grab.com","vismaifood.com","assets.grab.com","images.solopos.com","ecs7.tokopedia.net",
-      "cdn.yummy.co.id","image.akurat.co","statics.indozone.news","d1vbn70lmn1nqe.cloudfront.net",
-      "kurio-img.kurioapps.com","sicmtechmesin.com","img.qraved.co","img.qraved.co","www.dapurkintamani.com",
-      "blog.tokowahab.com","www.sugarandsoul.co/","images.immediate.co.uk", "www.sugarandsoul.co",
-      "vismaifood.com", "cdn0-production-images-kly.akamaized.net", "www.rukita.co", "kbu-cdn.com",
-      "asset.kompas.com", "i0.wp.com", "cdn-cas.orami.co.id", "st.depositphotos.com","i0.wp.com",
-      "www.chewoutloud.com", "www.blenderbabes.com", "decoratedtreats.com", "www.hersheyland.com",
-      "assets.digination.id","selerasa.com","firebasestorage.googleapis.com"
+			"firebasestorage.googleapis.com"
 		],
 	},
 };
