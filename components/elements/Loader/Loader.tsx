a2c456feae29ec1be3a22654ddd59b119d792af6
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-25 12:38:22 +0700

    create loader file

---
+++ components/elements/Loader/Loader.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Loader = () => {
+	return (
+		<div className="cascade mx-auto my-20">
+			<div>⬜</div>
+			<div>⬜</div>
+			<div>⬜</div>
+			<div>⬜</div>
+			<div>⬜</div>
+		</div>
+	);
+};
+
+export default Loader;
