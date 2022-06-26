2cfa8abb312af24f348d71cf019e35549a9204df
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-26 11:27:46 +0700

    reformat modifier button

--- components/elements/Button/ModifierButton.tsx
+++ components/elements/Button/ModifierButton.tsx
@@ -3,21 +3,26 @@ import DecrementButton from "./DecrementButton";
 import IncrementButton from "./IncrementButton";
 import { ModifierButtonProps } from "./interface";
 
-const ModifierButton: React.FC<ModifierButtonProps> = ({ setIsModifierButtonOpen }) => {
-    const [num, setNum] = useState<number>(1)
+const ModifierButton: React.FC<ModifierButtonProps> = ({
+	setIsModifierButtonOpen,
+}) => {
+	const [num, setNum] = useState<number>(1);
 
-    const handleDecrement = () => {
-        if (num > 1) {
-            setNum(num - 1)
-        } else {
-            setIsModifierButtonOpen(false)
-        }
-    }
+	const handleDecrement = () => {
+		if (num > 1) {
+			setNum(num - 1);
+		} else {
+			setIsModifierButtonOpen(false);
+		}
+	};
 
 	return (
-		<div className="flex justify-center p-2.5 rounded-lg bg-violet-700 text-base sm:text-lg shadow-xl gap-3 w-1/2 font-semibold">
-            <DecrementButton onClick={handleDecrement} />
-            {num}
+		<div
+			className="flex justify-center p-2.5 rounded-lg bg-violet-700 
+            text-base sm:text-lg shadow-xl gap-3 w-1/2 font-semibold"
+		>
+			<DecrementButton onClick={handleDecrement} />
+			<span>{num}</span>
 			<IncrementButton onClick={() => setNum(num + 1)} />
 		</div>
 	);
