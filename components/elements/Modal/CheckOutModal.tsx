6d01582326f7e166f9b6ad7f0b2c7f44d76f7ed4
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-06-26 12:43:34 +0700

    reformat check out modal

--- components/elements/Modal/CheckOutModal.tsx
+++ components/elements/Modal/CheckOutModal.tsx
@@ -2,31 +2,45 @@ import React from "react";
 import { CheckoutModalProps } from "./interface";
 
 const CheckOutModal: React.FC<CheckoutModalProps> = ({ setIsModalOpen }) => {
-    
+	const handleOrder = () => {
+		// TODO: implement store to database
+	};
+
 	return (
-		<>  
-            <input type="checkbox" id="confirmationModal" className="modal-toggle" />
-            
+		<>
+			<input
+				type="checkbox"
+				id="confirmationModal"
+				className="modal-toggle"
+			/>
+
 			<div className="modal">
 				<div className="modal-box flex flex-col gap-4">
 					<h3 className="font-bold text-lg uppercase">
 						Checkout Confirmation
 					</h3>
 					<p>
-                        Please check again the number of products that you wanna buy because it won&apos;t
-                        be able to change after you click the Order button
-                    </p>
-                    
-                    <p className="text-base">
-                        Are you sure wanna order that product?
+						Please check again the number of products that you wanna
+						buy because it won&apos;t be able to change after you
+						click the Order button
+					</p>
+
+					<p className="text-base">
+						Are you sure wanna order that product?
 					</p>
 					<div className="modal-action">
-                        <div onClick={() => setIsModalOpen(false)} className="btn btn-outline btn-secondary">
+						<div
+							onClick={() => setIsModalOpen(false)}
+							className="btn btn-outline btn-secondary"
+						>
 							Cancel
 						</div>
-						<label htmlFor="confirmationModal" className="btn btn-outline btn-primary">
+						<div
+							onClick={handleOrder}
+							className="btn btn-outline btn-primary"
+						>
 							Order
-                        </label>
+						</div>
 					</div>
 				</div>
 			</div>
