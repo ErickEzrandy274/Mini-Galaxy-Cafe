7c4b334fbd130bbdbc699c6f3ce2ba4c0f11a8da
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-03 20:27:18 +0700

    remove unused tag

--- components/modules/ProductPage/ProductPage.tsx
+++ components/modules/ProductPage/ProductPage.tsx
@@ -11,18 +11,11 @@ const ProductPage: React.FC<LandingPageProps> = ({ data, type }) => {
 	return (
 		<FavContextProvider>
 			<div className="flex flex-col md:flex-row mx-10 gap-5 md:gap-0">
-				<div className="md:hidden flex flex-col justify-center items-center">
-					<CheckOutButton onClick={() => setIsModalOpen(true)} />
-					{isModalOpen && (
-						<CheckOutModal setIsModalOpen={setIsModalOpen} />
-					)}
-				</div>
-
 				<h2 className="text-white text-5xl lg:text-6xl mx-auto md:text-right tracking-wider font-semibold md:w-2/3">
 					Our {type}...
 				</h2>
 				
-				<div className="hidden md:flex flex-col justify-center items-end w-1/3">
+				<div className="flex flex-col justify-center items-center md:items-end md:w-1/3">
 					<CheckOutButton onClick={() => setIsModalOpen(true)} />
 					{isModalOpen && (
 						<CheckOutModal setIsModalOpen={setIsModalOpen} />
