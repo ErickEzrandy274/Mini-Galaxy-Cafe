d08f60bb5d418a8418f3a35e2d7b95171e051585
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-05 12:01:15 +0700

    make title center on mobile version

--- components/modules/ProductPage/ProductPage.tsx
+++ components/modules/ProductPage/ProductPage.tsx
@@ -4,17 +4,26 @@ import CheckOutModal from "../../elements/Modal/CheckOutModal";
 import { LandingPageProps } from "./interface";
 import { FavContextProvider } from "../../../context/FavContext";
 import CheckOutButton from "../../elements/Button/CheckOutButton";
+import { useWindowSize } from "../../utils/function/useWindowSize";
 
 const ProductPage: React.FC<LandingPageProps> = ({ data, type }) => {
 	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+	const { width } = useWindowSize();
 
 	return (
 		<FavContextProvider>
 			<div className="flex flex-col md:flex-row mx-10 gap-5 md:gap-0">
 				<h2 className="text-white text-5xl lg:text-6xl mx-auto md:text-right tracking-wider font-semibold md:w-2/3">
-					Our {type}...
+					{width < 400 ? (
+						<div className="text-center">
+							<p>Our</p>
+							<p>{type}...</p>
+						</div>
+					) : (
+						<p>Our {type}...</p>
+					)}
 				</h2>
-				
+
 				<div className="flex flex-col justify-center items-center md:items-end md:w-1/3">
 					<CheckOutButton onClick={() => setIsModalOpen(true)} />
 					{isModalOpen && (
