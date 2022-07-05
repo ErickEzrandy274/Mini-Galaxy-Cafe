091a1b2a2f8c9198062592dafb8ac984ce093cbb
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-05 13:48:24 +0700

    fix styling title on product page

--- components/modules/ProductPage/ProductPage.tsx
+++ components/modules/ProductPage/ProductPage.tsx
@@ -4,24 +4,20 @@ import CheckOutModal from "../../elements/Modal/CheckOutModal";
 import { LandingPageProps } from "./interface";
 import { FavContextProvider } from "../../../context/FavContext";
 import CheckOutButton from "../../elements/Button/CheckOutButton";
-import { useWindowSize } from "../../utils/function/useWindowSize";
 
 const ProductPage: React.FC<LandingPageProps> = ({ data, type }) => {
 	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-	const { width } = useWindowSize();
 
 	return (
 		<FavContextProvider>
-			<div className="flex flex-col md:flex-row mx-auto md:mx-10 gap-5 md:gap-0">
-				<h2 className="text-white text-4xl sm:text-5xl lg:text-6xl mx-auto md:text-right tracking-wider font-semibold md:w-2/3">
-					{width < 400 ? (
-						<div className="text-center">
-							<p>Our</p>
-							<p>{type}...</p>
-						</div>
-					) : (
-						<p>Our {type}...</p>
-					)}
+			<div className="flex flex-col md:flex-row mx-auto md:mx-10 gap-5 md:gap-0 font-semibold">
+				<h2 className="hidden sm:block text-white text-6xl mx-auto md:text-right tracking-wider md:w-2/3">
+					<p>Our {type}...</p>
+				</h2>
+
+				<h2 className="sm:hidden text-center text-white text-5xl mx-auto tracking-wider">
+					<p>Our</p>
+					<p>{type}...</p>
 				</h2>
 
 				<div className="flex flex-col justify-center items-center md:items-end md:w-1/3">
