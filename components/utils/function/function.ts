8cb55bfa319e8fc54b316ecec7d2ca3bd002bbcc
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-05 16:20:32 +0700

    remove fetch data function from django

--- components/utils/function/function.ts
+++ components/utils/function/function.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import {
 	LoginInputType,
 	TypeForm,
@@ -13,27 +12,6 @@ export type FeedbackType = {
 	disable: boolean;
 	message: string;
 };
-
-let typeX = "";
-
-export const setType = (type: String) => {
-	typeX =
-		type === "food"
-			? "findFoodAll"
-			: type === "beverage"
-			? "findBeverageAll"
-			: "findSnackAll";
-};
-
-export async function getData() {
-	const { data } = await axios.get(
-		`https://mini-galaxy-cafe.herokuapp.com/data/${typeX}`
-	);
-
-	return {
-		props: { data },
-	};
-}
 
 export const extractError = (err: any) => {
 	const { code } = err;
