7db9e494aea13586e86c14446ef975d595788628
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-05 14:44:02 +0700

    show information that no fav product in current user

--- components/modules/FavoritePage/FavoriteWrapper.tsx
+++ components/modules/FavoritePage/FavoriteWrapper.tsx
@@ -6,6 +6,8 @@ import { useAuth } from "../../../context/AuthContext";
 
 const FavoriteWrapper: React.FC<{ data: any }> = ({ data }) => {
 	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+	const noData =
+		data.listFavItem === undefined || data.listFavItem.length === 0;
 
 	const {
 		user: { displayName },
@@ -13,19 +15,29 @@ const FavoriteWrapper: React.FC<{ data: any }> = ({ data }) => {
 	return (
 		<>
 			<div className="flex flex-col md:flex-row mx-10 gap-5 md:gap-0">
-				<h2 className="text-white text-5xl lg:text-6xl mx-auto md:text-right tracking-wider font-semibold md:w-2/3">
+				<h2
+					className={`hidden sm:block text-white text-5xl lg:text-6xl mx-auto tracking-wider font-semibold 
+					${noData ? `w-full text-center` : `md:w-2/3 md:text-right`}`}
+				>
 					{displayName} Favorites...
 				</h2>
 
-				<div className="flex flex-col justify-center items-center md:items-end md:w-1/3">
-					<CheckOutButton onClick={() => setIsModalOpen(true)} />
-					{isModalOpen && (
-						<CheckOutModal setIsModalOpen={setIsModalOpen} />
-					)}
-				</div>
+				<h2 className="sm:hidden text-white text-center text-4xl mx-auto tracking-wider font-semibold">
+					<p>{displayName}</p>
+					<p>Favorites...</p>
+				</h2>
+
+				{!noData && (
+					<div className="flex flex-col justify-center items-center md:items-end md:w-1/3">
+						<CheckOutButton onClick={() => setIsModalOpen(true)} />
+						{isModalOpen && (
+							<CheckOutModal setIsModalOpen={setIsModalOpen} />
+						)}
+					</div>
+				)}
 			</div>
-			{data.length === 0 || data.listFavItem.length === 0 ? (
-				<div className="flex flex-col gap-5 text-center text-lg sm:text-6xl text-gray-300 my-10">
+			{noData ? (
+				<div className="flex flex-col gap-5 text-center text-4xl sm:text-5xl lg:text-6xl text-gray-300 my-10">
 					<h2>No favorites product</h2>
 					<p>Select it first!</p>
 				</div>
@@ -34,7 +46,7 @@ const FavoriteWrapper: React.FC<{ data: any }> = ({ data }) => {
 					{data?.listFavItem?.map((item: any) => {
 						return (
 							<ProductCard
-								key={item.type + "-" + item.id}
+								key={item.type + "-" + item.dataId}
 								{...item}
 								type={item.type}
 							/>
