fc151c0a450b435154cbf3916e6a3946fe80c773
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-05 09:58:29 +0700

    add icons and fix styling on check out modal

--- components/elements/Modal/CheckOutModal.tsx
+++ components/elements/Modal/CheckOutModal.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWarning } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import { CheckoutModalProps } from "./interface";
 
@@ -15,17 +17,20 @@ const CheckOutModal: React.FC<CheckoutModalProps> = ({ setIsModalOpen }) => {
 			/>
 
 			<div className="modal text-gray-300">
-				<div className="modal-box flex flex-col gap-4">
-					<h3 className="font-bold text-lg uppercase">
-						Checkout Confirmation
-					</h3>
+				<div className="modal-box flex flex-col gap-4 shadow-2xl shadow-gray-300/75">
+					<div className="flex gap-2 items-center">
+						<FontAwesomeIcon icon={faWarning} className="w-7" />
+						<h3 className="font-bold text-lg uppercase">
+							Checkout Confirmation
+						</h3>
+					</div>
 					<p>
 						Please check again the number of products that you wanna
 						buy because it won&apos;t be able to change after you
 						click the Order button
 					</p>
 
-					<p className="text-base">
+					<p>
 						Are you sure wanna order that product?
 					</p>
 					<div className="modal-action">
