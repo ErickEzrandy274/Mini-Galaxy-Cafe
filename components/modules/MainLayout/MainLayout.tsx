9638f13fea2e062ef1a717b2ba59d90c7826b44c
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-05 16:18:09 +0700

    add register and login path to give gap on main layout

--- components/modules/MainLayout/MainLayout.tsx
+++ components/modules/MainLayout/MainLayout.tsx
@@ -2,15 +2,18 @@ import { useRouter } from "next/router";
 import React from "react";
 import Footer from "../Footer/Footer";
 import Navbar from "../Header/Navbar";
-import { MainLayoutProps } from "./interface";
+import { MainLayoutProps, nav } from "./interface";
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-	const { pathname } = useRouter()
-	const nav: string[] = ["/", "/food", "/beverage", "/snack", "/favorite", "/checkout"]
-	const registeredLink: boolean = nav.includes(pathname)
-	
+	const { pathname } = useRouter();
+	const registeredLink: boolean = nav.includes(pathname);
+
 	return (
-		<div className={`flex flex-col justify-between ${registeredLink ? `gap-5` : `gap-0`} bg-gray-700 min-h-screen`}>
+		<div
+			className={`flex flex-col justify-between ${
+				registeredLink ? `gap-5` : `gap-0`
+			} bg-gray-700 min-h-screen`}
+		>
 			<Navbar />
 			{children}
 			<Footer />
