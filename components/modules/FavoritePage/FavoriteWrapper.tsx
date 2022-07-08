c783dcb7a17a431e92471c668189c6e820624045
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-08 11:41:09 +0700

    fix favorite wrapper on using redux store

--- components/modules/FavoritePage/FavoriteWrapper.tsx
+++ components/modules/FavoritePage/FavoriteWrapper.tsx
@@ -3,9 +3,12 @@ import ProductCard from "../../elements/Card/ProductCard";
 import CheckOutModal from "../../elements/Modal/CheckOutModal";
 import CheckOutButton from "../../elements/Button/CheckOutButton";
 import { useAuth } from "../../../context/AuthContext";
+import { BuyerProduct } from "../../elements/Card/interface";
+import { useSelector } from "react-redux";
 
 const FavoriteWrapper: React.FC<{ data: any }> = ({ data }) => {
 	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+	const productList: BuyerProduct[] = useSelector((state: any) => state.buyerProduct.productList)
 	const noData =
 		data.listFavItem === undefined || data.listFavItem.length === 0;
 
@@ -14,7 +17,7 @@ const FavoriteWrapper: React.FC<{ data: any }> = ({ data }) => {
 	} = useAuth();
 	return (
 		<>
-			<div className="flex flex-col md:flex-row mx-10 gap-5 md:gap-0">
+			<div className="flex flex-col md:flex-row mx-10 gap-5 md:gap-0 font-semibold">
 				<h2
 					className={`hidden sm:block text-white text-5xl lg:text-6xl mx-auto tracking-wider font-semibold 
 					${noData ? `w-full text-center` : `md:w-2/3 md:text-right`}`}
@@ -28,12 +31,12 @@ const FavoriteWrapper: React.FC<{ data: any }> = ({ data }) => {
 				</h2>
 
 				{!noData && (
-					<div className="flex flex-col justify-center items-center md:items-end md:w-1/3">
-						<CheckOutButton onClick={() => setIsModalOpen(true)} />
-						{isModalOpen && (
-							<CheckOutModal setIsModalOpen={setIsModalOpen} />
-						)}
-					</div>
+					<div className="flex flex-col justify-center items-center md:items-center md:w-1/3">
+					<CheckOutButton onClick={() => setIsModalOpen(true)} productList={productList} />
+					{isModalOpen && (
+						<CheckOutModal setIsModalOpen={setIsModalOpen} productList={productList} />
+					)}
+				</div>
 				)}
 			</div>
 			{noData ? (
