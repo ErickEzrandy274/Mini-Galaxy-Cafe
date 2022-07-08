429feec9b09ae0507d5ce75f2abc69ba481edd30
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-08 11:43:52 +0700

    handle checkout button where there is no buyer products

--- components/elements/Button/CheckOutButton.tsx
+++ components/elements/Button/CheckOutButton.tsx
@@ -6,7 +6,7 @@ const CheckOutButton: React.FC<CheckOutModalButtonProps> = ({
 	onClick,
 	productList,
 }) => {
-	const isEmpty = productList.length === 0 ? true : false;
+	const isEmpty = productList === undefined ? true : productList.length === 0;
 
 	return (
 		<>
@@ -14,7 +14,9 @@ const CheckOutButton: React.FC<CheckOutModalButtonProps> = ({
 				htmlFor={isEmpty ? `` : `confirmationModal`}
 				onClick={onClick}
 				className={`btn modal-button text-lg shadow-xl w-36 text-base-200 text-center ${
-					isEmpty ? `cursor-not-allowed bg-gray-400 hover:bg-gray-400` : `btn-primary hover:text-white`
+					isEmpty
+						? `cursor-not-allowed bg-gray-400 hover:bg-gray-400 border-none`
+						: `btn-primary hover:text-white`
 				}`}
 			>
 				Check Out
