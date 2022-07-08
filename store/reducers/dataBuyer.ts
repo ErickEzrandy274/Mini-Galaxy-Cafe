be905e4563d09e97e185189d3fde824d0f9f2f5c
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-08 10:41:45 +0700

    remove console log

--- store/reducers/dataBuyer.ts
+++ store/reducers/dataBuyer.ts
@@ -1,52 +1,52 @@
-import { ProductCardProps } from "../../components/elements/Card/interface";
+import { BuyerProduct } from "../../components/elements/Card/interface";
 import { ADD_PRODUCT, DELETE_PRODUCT } from "../types";
-
-interface BuyerProduct extends ProductCardProps {
-    amount: number
-}
 
 const initialState: { productList: BuyerProduct[] } = {
 	productList: [],
 };
 
-const dataBuyerReducer = (state = initialState, action: {type: string, payload: BuyerProduct}) => {
+const dataBuyerReducer = (
+	state = initialState,
+	action: { type: string; payload: BuyerProduct }
+) => {
 	const { type, payload } = action;
-    const { productList } = state
-    const index = productList.findIndex(({ dataId }) => dataId === payload.dataId)
+	const { productList } = state;
+	const index = productList.findIndex(
+		({ dataId }) => dataId === payload.dataId
+	);
 
-    switch (type) {
-        case ADD_PRODUCT:
+	switch (type) {
+		case ADD_PRODUCT:
+			if (index === -1) {
+				const newProductList = [...productList, payload];
 
-            if (index === -1) {
-                const newProductList = [...productList, payload]
-                
-                return {
-                    ...state,
-                    productList: newProductList
-                }
-            } else {
-                productList[index] = { ...payload }
-                return {
-                    ...state,
-                }
-            }
+				return {
+					...state,
+					productList: newProductList,
+				};
+			} else {
+				productList[index] = { ...payload };
+				return {
+					...state,
+				};
+			}
 
-        case DELETE_PRODUCT:
-            const { amount } = payload
-            console.log(amount)
-            if (amount === 0) {
-                const newProductList = productList.filter(({ dataId }) => dataId !== payload.dataId)
-                console.log(newProductList)
-                return {
-                    ...state,
-                    productList: newProductList
-                }
-            } else {
-                productList[index] = { ...payload }
-                return {
-                    ...state,
-                }
-            }
+		case DELETE_PRODUCT:
+			const { amount } = payload;
+			if (amount === 0) {
+				const newProductList = productList.filter(
+					({ dataId }) => dataId !== payload.dataId
+				);
+				return {
+					...state,
+					productList: newProductList,
+				};
+			} else {
+				productList[index] = { ...payload };
+				return {
+					...state,
+				};
+			}
 
 		default:
 			return state;
