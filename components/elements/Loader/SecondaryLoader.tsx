7521c6b52cb19a3daacae75c61bcbc18d33afece
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-11 19:29:00 +0700

    fix secondary loader styling

--- components/elements/Loader/SecondaryLoader.tsx
+++ components/elements/Loader/SecondaryLoader.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const SecondaryLoader = () => {
 	return (
-		<div className="bg-gray-100/10 flex-1 p-32 rounded-lg relative">
 			<div className="dots-flow"></div>
-		</div>
 	);
 };
 
--- components/modules/CheckOut/CheckOut.tsx
+++ components/modules/CheckOut/CheckOut.tsx
@@ -38,71 +38,65 @@ const CheckOut: React.FC<CheckOutProps> = ({ data }) => {
 
 	return (
 		<div className="mx-4">
-			{isLoading ? (
-				<SecondaryLoader />
-			) : (
-				<div className="bg-gray-100/10 flex-1 p-4 rounded-lg">
-					{newData !== undefined && newData.length > 0 && !isPayed ? (
-						<>
-							<h1 className="font-semibold text-4xl">
-								Your Cart
-							</h1>
+			<div className="bg-gray-100/10 flex-1 p-4 rounded-lg">
+				{newData !== undefined && newData.length > 0 && !isPayed ? (
+					<>
+						<h1 className="font-semibold text-4xl">Your Cart</h1>
 
-							<div className="mt-4 rounded-md overflow-x-auto shadow-md">
-								<CheckoutTable products={newData} />
-							</div>
-
-							<div className="w-full flex flex-col items-end space-y-4 mt-4 p-2 text-white font-semibold">
-								<div className="flex justify-between w-60">
-									<p>Subtotal</p>
-									<p>Rp {makeRupiahValue(subtotal)}</p>
-								</div>
-
-								<div className="flex justify-between w-60">
-									<p>Fees and Taxes</p>
-									<p>Rp {makeRupiahValue(tax)}</p>
-								</div>
-
-								<div className="flex justify-between w-60">
-									<p>Total</p>
-									<p>Rp {makeRupiahValue(subtotal + tax)}</p>
-								</div>
-
-								<div className="flex flex-col justify-center items-center md:items-center">
-									<div>
-										<ModalButton
-											to="Payment"
-											onClick={() => setIsModalOpen(true)}
-										/>
-									</div>
-
-									{isModalOpen && (
-										<CheckOutModal
-											type="Payment"
-											setIsModalOpen={setIsModalOpen}
-											handlePayment={handlePayment}
-										/>
-									)}
-								</div>
-							</div>
-						</>
-					) : (
-						<div className="flex flex-col gap-5 text-center text-4xl sm:text-5xl lg:text-6xl text-gray-300 my-10">
-							{isPayed ? (
-								<>
-									<h2>Thank you for buying our dish</h2>
-									<p>Enjoy your dish 😁😁😁</p>
-								</>
-							) : (
-								<>
-									<h2>No products in your cart</h2>
-									<p>Select it first!</p>
-								</>
-							)}
+						<div className="mt-4 rounded-md overflow-x-auto shadow-md">
+							<CheckoutTable products={newData} />
 						</div>
-					)}
-				</div>
-			)}
+
+						<div className="w-full flex flex-col items-end space-y-4 mt-4 p-2 text-white font-semibold">
+							<div className="flex justify-between w-60">
+								<p>Subtotal</p>
+								<p>Rp {makeRupiahValue(subtotal)}</p>
+							</div>
+
+							<div className="flex justify-between w-60">
+								<p>Fees and Taxes</p>
+								<p>Rp {makeRupiahValue(tax)}</p>
+							</div>
+
+							<div className="flex justify-between w-60">
+								<p>Total</p>
+								<p>Rp {makeRupiahValue(subtotal + tax)}</p>
+							</div>
+
+							<div className="flex flex-col justify-center items-center md:items-center">
+								<div>
+									<ModalButton
+										to="Payment"
+										onClick={() => setIsModalOpen(true)}
+									/>
+								</div>
+
+								{isModalOpen && (
+									<CheckOutModal
+										type="Payment"
+										setIsModalOpen={setIsModalOpen}
+										handlePayment={handlePayment}
+									/>
+								)}
+							</div>
+						</div>
+					</>
+				) : (
+					<div className="flex flex-col gap-5 text-center text-4xl sm:text-5xl lg:text-6xl text-gray-300 my-10 relative">
+						{isLoading ? <SecondaryLoader /> : isPayed ? (
+							<>
+								<h2>Thank you for buying our dish</h2>
+								<p>Enjoy your dish 😁😁😁</p>
+							</>
+						) : (
+							<>
+								<h2>No products in your cart</h2>
+								<p>Select it first!</p>
+							</>
+						)}
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
