bab3d00c4878b71aaef226ddbeea845ec094663b
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-11 18:51:28 +0700

    create second loader for handling payment

---
+++ components/elements/Loader/SecondaryLoader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SecondaryLoader = () => {
+	return (
+		<div className="bg-gray-100/10 flex-1 p-32 rounded-lg relative">
+			<div className="dots-flow"></div>
+		</div>
+	);
+};
+
+export default SecondaryLoader;
--- components/modules/CheckOut/CheckOut.tsx
+++ components/modules/CheckOut/CheckOut.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { useAuth } from "../../../context/AuthContext";
 import { reset_product } from "../../../store/actions/action";
 import ModalButton from "../../elements/Button/CheckOutButton";
+import SecondaryLoader from "../../elements/Loader/SecondaryLoader";
 import CheckOutModal from "../../elements/Modal/CheckOutModal";
 import CheckoutTable from "../../elements/Table/CheckoutTable";
 import { deleteBuyerProduct } from "../../utils/function/dataManipulation";
@@ -13,6 +14,7 @@ const CheckOut: React.FC<CheckOutProps> = ({ data }) => {
 	let newData: any[] = data;
 	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 	const [isPayed, setIsPayed] = useState<boolean>(false);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const dispatch = useDispatch();
 	const {
 		user: { uid },
@@ -23,74 +25,84 @@ const CheckOut: React.FC<CheckOutProps> = ({ data }) => {
 	const tax: number = subtotal * 0.1;
 
 	const handlePayment = async () => {
+		setIsLoading(true);
 		newData = [];
 		setIsPayed(true);
 		await deleteBuyerProduct(uid);
-		setIsModalOpen(false)
-		dispatch(reset_product())
+		setIsModalOpen(false);
+		dispatch(reset_product());
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 1500);
 	};
 
 	return (
 		<div className="mx-4">
-			<div className="bg-gray-100/10 flex-1 p-4 rounded-lg">
-				{newData !== undefined && newData.length > 0 && !isPayed ? (
-					<>
-						<h1 className="font-semibold text-4xl">Your Cart</h1>
+			{isLoading ? (
+				<SecondaryLoader />
+			) : (
+				<div className="bg-gray-100/10 flex-1 p-4 rounded-lg">
+					{newData !== undefined && newData.length > 0 && !isPayed ? (
+						<>
+							<h1 className="font-semibold text-4xl">
+								Your Cart
+							</h1>
 
-						<div className="mt-4 rounded-md overflow-x-auto shadow-md">
-							<CheckoutTable products={newData} />
-						</div>
-
-						<div className="w-full flex flex-col items-end space-y-4 mt-4 p-2 text-white font-semibold">
-							<div className="flex justify-between w-60">
-								<p>Subtotal</p>
-								<p>Rp {makeRupiahValue(subtotal)}</p>
+							<div className="mt-4 rounded-md overflow-x-auto shadow-md">
+								<CheckoutTable products={newData} />
 							</div>
 
-							<div className="flex justify-between w-60">
-								<p>Fees and Taxes</p>
-								<p>Rp {makeRupiahValue(tax)}</p>
-							</div>
-
-							<div className="flex justify-between w-60">
-								<p>Total</p>
-								<p>Rp {makeRupiahValue(subtotal + tax)}</p>
-							</div>
-
-							<div className="flex flex-col justify-center items-center md:items-center">
-								<div>
-									<ModalButton
-										to="Payment"
-										onClick={() => setIsModalOpen(true)}
-									/>
+							<div className="w-full flex flex-col items-end space-y-4 mt-4 p-2 text-white font-semibold">
+								<div className="flex justify-between w-60">
+									<p>Subtotal</p>
+									<p>Rp {makeRupiahValue(subtotal)}</p>
 								</div>
 
-								{isModalOpen && (
-									<CheckOutModal
-										type="Payment"
-										setIsModalOpen={setIsModalOpen}
-										handlePayment={handlePayment}
-									/>
-								)}
+								<div className="flex justify-between w-60">
+									<p>Fees and Taxes</p>
+									<p>Rp {makeRupiahValue(tax)}</p>
+								</div>
+
+								<div className="flex justify-between w-60">
+									<p>Total</p>
+									<p>Rp {makeRupiahValue(subtotal + tax)}</p>
+								</div>
+
+								<div className="flex flex-col justify-center items-center md:items-center">
+									<div>
+										<ModalButton
+											to="Payment"
+											onClick={() => setIsModalOpen(true)}
+										/>
+									</div>
+
+									{isModalOpen && (
+										<CheckOutModal
+											type="Payment"
+											setIsModalOpen={setIsModalOpen}
+											handlePayment={handlePayment}
+										/>
+									)}
+								</div>
 							</div>
+						</>
+					) : (
+						<div className="flex flex-col gap-5 text-center text-4xl sm:text-5xl lg:text-6xl text-gray-300 my-10">
+							{isPayed ? (
+								<>
+									<h2>Thank you for buying our dish</h2>
+									<p>Enjoy your dish 😁😁😁</p>
+								</>
+							) : (
+								<>
+									<h2>No products in your cart</h2>
+									<p>Select it first!</p>
+								</>
+							)}
 						</div>
-					</>
-				) : (
-					<div className="flex flex-col gap-5 text-center text-4xl sm:text-5xl lg:text-6xl text-gray-300 my-10">
-						{isPayed ? (
-							<>
-								<h2>Thank you for buying our dish</h2>
-								<p>Enjoy your dish 😁😁😁</p>
-							</>
-						) : (
-							<>
-								<h2>No products in your cart</h2>
-								<p>Select it first!</p>
-							</>
-						)}
-					</div>
-				)}
-			</div>
+					)}
+				</div>
+			)}
 		</div>
 	);
 };
