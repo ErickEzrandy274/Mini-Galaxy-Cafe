67d4657c391035579414dbd2ea47949b1576eda8
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-11 14:17:04 +0700

    add checkout interface and remove dummy data

--- components/modules/CheckOut/CheckOut.tsx
+++ components/modules/CheckOut/CheckOut.tsx
@@ -1,50 +1,38 @@
 import React from "react";
-import Button from "../../elements/Button/Button";
-import CheckoutTable, { Product } from "../../elements/Table/CheckoutTable";
+import CheckoutTable from "../../elements/Table/CheckoutTable";
+import { CheckOutProps } from "./interface";
 
-const products: Product[] = [
-	{
-		image: "gs:/mini-galaxy-cafe.appspot.com/Foods/Lasagna.jpg",
-		name: "Lasagna",
-		price: 45000,
-		productId: "312432j423ijij",
-		quantity: 1,
-		type: "Foods",
-	},
-	{
-		image: "gs:/mini-galaxy-cafe.appspot.com/Foods/Lasagna.jpg",
-		name: "Lasagna",
-		price: 25000,
-		productId: "312dsadvvfdj",
-		quantity: 3,
-		type: "Foods",
-	},
-];
+const CheckOut: React.FC<CheckOutProps> = ({ data }) => {
+	const subtotal = data.reduce((acc, item) => {
+		return acc + item.price;
+	}, 0);
+	const tax: number = subtotal * 0.1;
 
-const CheckOut = () => {
 	return (
 		<div className="mx-4">
 			<div className="bg-gray-100/10 flex-1 p-4 rounded-lg">
-				<h1 className="font-semibold text-xl">Your Cart</h1>
+				<h1 className="font-semibold text-4xl">Your Cart</h1>
 
 				<div className="mt-4 rounded-md overflow-x-auto shadow-md">
-					<CheckoutTable products={products} />
+					<CheckoutTable products={data} />
 				</div>
 
 				<div className="w-full flex flex-col items-end space-y-4 mt-4 p-2">
 					<div className="flex justify-between w-60">
 						<p>Subtotal</p>
-						<p>Rp 50.000</p>
+						<p>Rp {subtotal}</p>
 					</div>
 					<div className="flex justify-between w-60">
 						<p>Fees and Taxes</p>
-						<p>Rp 10.000</p>
+						<p>Rp {tax}</p>
 					</div>
 					<div className="flex justify-between w-60">
 						<p>Total</p>
-						<p>Rp 60.000</p>
+						<p>Rp {subtotal + tax}</p>
 					</div>
-					<button className="btn text-base sm:text-md btn-primary shadow-xl text-base-200 hover:text-white">Pay</button>
+					<button className="btn text-base sm:text-md btn-primary shadow-xl text-base-200 w-20 hover:text-white">
+						Pay
+					</button>
 				</div>
 			</div>
 		</div>
