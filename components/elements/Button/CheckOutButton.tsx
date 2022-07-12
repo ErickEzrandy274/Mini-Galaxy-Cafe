bda83037ceadca6b0eea07403109ad7f8be263d4
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-12 10:40:07 +0700

    implement animation on check out button and tool tip info

--- components/elements/Button/CheckOutButton.tsx
+++ components/elements/Button/CheckOutButton.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import InfoTooltip from "../Tooltip/InfoTooltip";
 import { CheckOutModalButtonProps } from "./interface";
+import { motion } from "framer-motion";
+import { buttonCheckOutAnimation } from "../../utils/animation/ProductPageAnimation";
 
 const ModalButton: React.FC<CheckOutModalButtonProps> = ({
 	onClick,
@@ -10,10 +12,15 @@ const ModalButton: React.FC<CheckOutModalButtonProps> = ({
 }) => {
 	const isEmpty = to === "Checkout" && productList!.length === 0;
 	const htmlFor = to === "Payment" ? `confirmationModal` : isEmpty ? `` : `confirmationModal`
+	const { initial, animate, exit, transition } = buttonCheckOutAnimation;
 
 	return (
 		<>
-			<label
+			<motion.label
+				initial={initial}
+				animate={animate}
+				exit={exit}
+				transition={transition}
 				htmlFor={htmlFor}
 				onClick={onClick}
 				className={`btn modal-button text-lg shadow-xl w-36 text-base-200 text-center ${
@@ -23,7 +30,7 @@ const ModalButton: React.FC<CheckOutModalButtonProps> = ({
 				}`}
 			>
 				{to === "Checkout" ? `Check Out` : `Pay`}
-			</label>
+			</motion.label>
 			{to === "Checkout" && <InfoTooltip type={type} productList={productList!} />}
 		</>
 	);
--- components/elements/Tooltip/InfoTooltip.tsx
+++ components/elements/Tooltip/InfoTooltip.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { TooltipProps } from "./interface";
+import { motion } from "framer-motion";
+import { menutitleAnimation } from "../../utils/animation/MenuPageAnimation";
 
 const InfoTooltip: React.FC<TooltipProps> = ({ productList, type }) => {
+	const { initial, animate, exit, transition } = menutitleAnimation;
 	const info: string =
 		productList.length > 0
 			? `Finish your order!`
@@ -9,13 +12,17 @@ const InfoTooltip: React.FC<TooltipProps> = ({ productList, type }) => {
 					type
 						? type.substring(0, type.length - 1).toLowerCase()
 						: `dish`
-			} to enable this button!`;
-	
+			  } to enable this button!`;
+
 	return (
-		<div
+		<motion.div
+			initial={initial}
+			animate={animate}
+			exit={exit}
+			transition={{...transition, delay: 1.25}}
 			className="tooltip tooltip-bottom tooltip-open tooltip-info"
 			data-tip={info}
-		></div>
+		></motion.div>
 	);
 };
 
