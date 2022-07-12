bd9d0ac71ff0dbf598ecb0b79289fb3799471074
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-07-12 14:11:11 +0700

    remove reloadWarning on food, beverage, n snack pages

--- pages/food.tsx
+++ pages/food.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import PrimaryLoader from "../components/elements/Loader/PrimaryLoader";
 import ProductPage from "../components/modules/ProductPage/ProductPage";
 import { getData } from "../components/utils/function/dataManipulation";
-import { reloadWarning } from "../components/utils/function/function";
 
 const food = () => {
 	const [data, setData] = useState<any[]>([]);
@@ -10,7 +9,6 @@ const food = () => {
 
 	useEffect(() => {
 		getData({ type: "Foods", setData, setIsLoading });
-		reloadWarning()
 	}, []);
 
 	return isLoading ? <PrimaryLoader /> : <ProductPage data={data} type="Foods" />;
