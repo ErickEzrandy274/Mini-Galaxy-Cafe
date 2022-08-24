9af5adf89f08ac75e51ef9496cdead1253769cc1
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-08-24 21:09:58 +0700

    add reference link

--- components/utils/api.ts
+++ components/utils/api.ts
@@ -1,3 +1,5 @@
+//  reference: https://nocodeapi.com/collect-website-data-with-react-and-airtable-nocodeapi
+
 export const BASE_URL =
 	"https://v1.nocodeapi.com/erickezrandy274/fbsdk/XZLzexRbXCsyMSBQ/firestore/allDocuments?collectionName=";
 
