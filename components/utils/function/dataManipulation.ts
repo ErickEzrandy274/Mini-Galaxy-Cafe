4a0d44d74753b493f20ad20b226a58792430ccd6
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-08-24 20:57:14 +0700

    remove getData method and dataType

--- components/utils/function/dataManipulation.ts
+++ components/utils/function/dataManipulation.ts
@@ -1,44 +1,7 @@
-import {
-	collection,
-	deleteDoc,
-	doc,
-	getDoc,
-	getDocs,
-	setDoc,
-} from "firebase/firestore";
+import { deleteDoc, doc, getDoc, setDoc } from "firebase/firestore";
 import React, { SetStateAction } from "react";
 import { BuyerProduct } from "../../elements/Card/interface";
-import { database, imageRef } from "../firebase/firebase";
-
-type DataFetchType = {
-	type: "Foods" | "Beverages" | "Snacks";
-	setData: React.Dispatch<SetStateAction<any[]>>;
-	setIsLoading: React.Dispatch<SetStateAction<boolean>>;
-};
-
-export const getData = async (item: DataFetchType) => {
-	const { type, setData, setIsLoading } = item;
-	setIsLoading(true);
-	const tempData: any[] = [];
-
-	const querySnapshot = await getDocs(collection(database, type));
-	querySnapshot.forEach(async (doc) => {
-		const { name, price, image } = doc.data();
-		const img = await imageRef(type, image._key.path.segments[8]);
-		const newData = {
-			dataId: doc.id,
-			name,
-			price,
-			image: img,
-		};
-		tempData.push(newData);
-	});
-
-	setData(tempData);
-	setTimeout(() => {
-		setIsLoading(false);
-	}, 2000);
-};
+import { database } from "../firebase/firebase";
 
 type handleFavType = {
 	fav: boolean;
