52cf6c7898243a05e1b423adbebf2a015b63cef5
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-08-25 12:34:42 +0700

    add warning when user is trying to reload the pages

--- pages/beverage.tsx
+++ pages/beverage.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import Head from "next/head";
-import React from "react";
+import React, { useEffect } from "react";
 import ProductPage from "../components/modules/ProductPage/ProductPage";
 import { BASE_URL, getHeaders } from "../components/utils/api";
 import { setData } from "../components/utils/function/dataManipulation";
+import { reloadWarning } from "../components/utils/function/function";
 
 export async function getServerSideProps() {
 	try {
@@ -19,6 +20,10 @@ export async function getServerSideProps() {
 }
 
 const beverage = ({ data }: any) => {
+	useEffect(() => {
+		reloadWarning();
+	}, []);
+
 	return (
 		<>
 			<Head>
