72cda21b796ea6171cc79fcb43aef74fc2de9167
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-08-25 13:05:54 +0700

    remove unused import

--- pages/snack.tsx
+++ pages/snack.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import Head from "next/head";
-import React, { useEffect } from "react";
+import React from "react";
 import ProductPage from "../components/modules/ProductPage/ProductPage";
 import { BASE_URL, getHeaders } from "../components/utils/api";
 import { setData } from "../components/utils/function/dataManipulation";
-import { reloadWarning } from "../components/utils/function/function";
 
 export async function getServerSideProps() {
 	try {
