4421487bfa92efa2246a887753281467f4752cb8
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-01-02 20:45:23 +0700

    export default price info constant

--- components/elements/PriceInfo/constant.ts
+++ components/elements/PriceInfo/constant.ts
@@ -1,1 +1,2 @@
-export const priceInfoConstant: string[] = ["Subtotal", "Fees and Taxes", "Total"];
+const priceInfoConstant: string[] = ["Subtotal", "Fees and Taxes", "Total"];
+export default priceInfoConstant;
