ac888faa0d1bcc566a034e3667f5f0511892382a
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-01-02 11:17:48 +0700

    create export default in table, auth, and header directory

---
+++ components/modules/Header/index.tsx
@@ -0,0 +1,5 @@
+export { default as Navbar } from "./Navbar";
+export { default as PreferredRoute } from "./PreferredRoute";
+export { default as UserDetected } from "./UserDetected";
+export * from "./constant";
+export * from "./interface";
