02143c2aee842ade53f960d8c71dd58b5cbe7ea5
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-01-02 11:15:26 +0700

    export default in button directory

---
+++ components/elements/Button/index.tsx
@@ -0,0 +1,9 @@
+export { default as Button } from "./Button";
+export { default as CheckOutButton } from "./CheckOutButton";
+export { default as DecrementButton } from "./DecrementButton";
+export { default as FavButton } from "./FavButton";
+export { default as IncrementButton } from "./IncrementButton";
+export { default as ModifierButton } from "./ModifierButton";
+export { default as ScrollButton } from "./ScrollButton";
+export * from "./constants";
+export * from "./interface";
