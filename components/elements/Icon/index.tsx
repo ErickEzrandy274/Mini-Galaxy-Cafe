42c2f7504e722506befb248d41d23ead627ca0cd
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-01-05 10:46:40 +0700

    export all icon components

--- components/elements/Icon/index.tsx
+++ components/elements/Icon/index.tsx
@@ -3,4 +3,5 @@ export { default as IconClose } from "./IconClose";
 export { default as IconFacebook } from "./IconFacebook";
 export { default as IconGithub } from "./IconGithub";
 export { default as IconGoogle } from "./IconGoogle";
+export { default as IconTrash } from "./IconTrash";
 export * from "./interface";
