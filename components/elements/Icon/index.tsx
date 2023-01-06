833d191dff46691f957435fe03f8d919ac97ca59
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-01-06 15:02:42 +0700

    export icon plus and icon minus component

--- components/elements/Icon/index.tsx
+++ components/elements/Icon/index.tsx
@@ -4,4 +4,6 @@ export { default as IconFacebook } from "./IconFacebook";
 export { default as IconGithub } from "./IconGithub";
 export { default as IconGoogle } from "./IconGoogle";
 export { default as IconTrash } from "./IconTrash";
+export { default as IconPlus } from "./IconPlus";
+export { default as IconMinus } from "./IconMinus";
 export * from "./interface";
