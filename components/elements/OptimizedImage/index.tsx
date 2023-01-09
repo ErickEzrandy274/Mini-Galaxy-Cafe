c27ce32c4eb03a7276012f26758b928c9b857fcf
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-01-09 20:05:29 +0700

    reformat structure code

--- components/elements/OptimizedImage/index.tsx
+++ components/elements/OptimizedImage/index.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import { OptimizedImageProps } from './interface'
-import Image from 'next/image'
+import React from "react";
+import { OptimizedImageProps } from "./interface";
+import Image from "next/image";
 
-const OptimizedImage: React.FC<OptimizedImageProps> = ({ image, index, welcoming }) => {
-    return (
-			<Image
-				src={image}
-				alt={"Foods-" + index}
-				width={500}
-				height={400}
-				objectFit="cover"
-				loading="eager"
-				className={welcoming ? `rounded-t-xl` : `rounded-lg`}
-			/>
-		);
-}
+const OptimizedImage: React.FC<OptimizedImageProps> = ({
+	image,
+	index,
+	welcoming,
+}) => {
+	return (
+		<Image
+			src={image}
+			alt={"Foods-" + index}
+			width={500}
+			height={400}
+			objectFit="cover"
+			loading="eager"
+			className={welcoming ? `rounded-t-xl` : `rounded-lg`}
+		/>
+	);
+};
 
-export default OptimizedImage
+export default OptimizedImage;
