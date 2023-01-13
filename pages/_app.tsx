7c45ee96acc2f06305de83d119f2bf25b48b80f7
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-01-13 13:54:33 +0700

    wrap all component using react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,12 +8,15 @@ import { PersistGate } from "redux-persist/integration/react";
 import { ScrollButton } from "@elements";
 import { persistor, wrapper } from "@reduxs";
 import { MainLayout, ProtectedRoute } from "@modules";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import Head from "next/head";
 import React from "react";
 
 export const noAuthRequired = ["/", "/login", "/register"];
 
 const MyApp: React.FC<AppProps> = ({ Component, ...rest }) => {
+	const [queryClient] = React.useState(() => new QueryClient());
 	const { pathname } = useRouter();
 	const {
 		store,
@@ -21,45 +24,56 @@ const MyApp: React.FC<AppProps> = ({ Component, ...rest }) => {
 	} = wrapper.useWrappedStore(rest);
 
 	return (
-		<AuthContextProvider>
-			<Head>
-				<title>Mini Galaxy Cafe</title>
-				<meta
-					name="keywords"
-					content="mini galaxy cafe, galaxy cafe erick, mini galaxy cafe erick ezrandy, cafe erick ezrandy"
-				/>
-				<meta name="author" content="Erick Ezrandy" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<link rel="preconnect" href="https://mini-galaxy-cafe.vercel.app/" />
-				<link rel="icon" href="/gal-logo.svg" />
-			</Head>
+		<QueryClientProvider client={queryClient}>
+			<Hydrate state={pageProps.dehydratedState}>
+				<AuthContextProvider>
+					<Head>
+						<title>Mini Galaxy Cafe</title>
+						<meta
+							name="keywords"
+							content="mini galaxy cafe, galaxy cafe erick, mini galaxy cafe erick ezrandy, cafe erick ezrandy"
+						/>
+						<meta name="author" content="Erick Ezrandy" />
+						<meta
+							name="viewport"
+							content="width=device-width, initial-scale=1.0"
+						/>
+						<link
+							rel="preconnect"
+							href="https://mini-galaxy-cafe.vercel.app/"
+						/>
+						<link rel="icon" href="/gal-logo.svg" />
+					</Head>
 
-			<Provider store={store}>
-				<UserStuffContextProvider>
-					{noAuthRequired.includes(pathname) ? (
-						pathname === "/" ? (
-							<>
-								<ScrollButton />
-								<Component {...pageProps} />
-							</>
-						) : (
-							<MainLayout>
-								<Component {...pageProps} />
-							</MainLayout>
-						)
-					) : (
-						<ProtectedRoute>
-							<ScrollButton />
-							<MainLayout>
-								<PersistGate loading={null} persistor={persistor}>
-									<Component {...pageProps} />
-								</PersistGate>
-							</MainLayout>
-						</ProtectedRoute>
-					)}
-				</UserStuffContextProvider>
-			</Provider>
-		</AuthContextProvider>
+					<Provider store={store}>
+						<UserStuffContextProvider>
+							{noAuthRequired.includes(pathname) ? (
+								pathname === "/" ? (
+									<>
+										<ScrollButton />
+										<Component {...pageProps} />
+									</>
+								) : (
+									<MainLayout>
+										<Component {...pageProps} />
+									</MainLayout>
+								)
+							) : (
+								<ProtectedRoute>
+									<ScrollButton />
+									<MainLayout>
+										<PersistGate loading={null} persistor={persistor}>
+											<Component {...pageProps} />
+										</PersistGate>
+									</MainLayout>
+								</ProtectedRoute>
+							)}
+						</UserStuffContextProvider>
+					</Provider>
+				</AuthContextProvider>
+				<ReactQueryDevtools initialIsOpen={false} />
+			</Hydrate>
+		</QueryClientProvider>
 	);
 };
 
