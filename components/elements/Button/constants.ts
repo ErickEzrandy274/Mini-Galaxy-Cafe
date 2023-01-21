bd554f5984932d5f0ed8189da67afa6c233d1365
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-01-21 11:58:42 +0700

    create checkingDisabledButton method

--- components/elements/Button/constants.ts
+++ components/elements/Button/constants.ts
@@ -1,2 +1,17 @@
 export const INITIAL_NUM = 1;
 export const RESET_NUM = 0;
+
+export const checkingDisabledButton = (error: boolean[]) => {
+	const isError = error.findIndex((item: boolean) => item === true);
+
+	return isError === -1
+		? {
+				additionClass:
+					"text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-400 focus:ring  focus:ring-blue-500 focus:ring-opacity-50 cursor-pointer",
+				disable: false,
+		  }
+		: {
+				additionClass: "text-gray-400 bg-gray-500 cursor-not-allowed",
+				disable: true,
+		  };
+};
