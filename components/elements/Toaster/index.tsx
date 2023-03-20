4bcdbe4e88926ff5ecb9a59742255464d1e7a690
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-03-20 11:45:35 +0700

    create bg-color for toaster component

--- components/elements/Toaster/index.tsx
+++ components/elements/Toaster/index.tsx
@@ -12,24 +12,31 @@ const index = () => {
 				error: { icon: <IconClose />, duration: 3000 },
 			}}
 		>
-			{(t) => (
-				<Transition
-					appear
-					show={t.visible}
-					className={`transform -mt-2.5 p-3 px-5 flex items-center gap-3 rounded-lg shadow-lg md:text-lg text-white font-semibold ${
-						t.type === "error" ? `bg-red-500` : `bg-green-500`
-					} tracking-wide`}
-					enter="transition-all duration-700"
-					enterFrom="opacity-0 ease-out scale-25"
-					enterTo="opacity-100 ease-in scale-100"
-					leave="transition-all duration-1000"
-					leaveFrom="opacity-100 scale-100"
-					leaveTo="opacity-0 scale-75"
-				>
-					<ToastIcon toast={t} />
-					<p>{t.message?.toString()}</p>
-				</Transition>
-			)}
+			{(t) => {
+				const backgroundColor =
+					t.type === "error"
+						? "bg-red-500"
+						: t.type === "success"
+						? "bg-green-500"
+						: "bg-gray-500";
+
+				return (
+					<Transition
+						appear
+						show={t.visible}
+						className={`transform -mt-2.5 p-3 px-5 flex items-center gap-3 rounded-lg shadow-lg md:text-lg text-white font-semibold ${backgroundColor} tracking-wide`}
+						enter="transition-all duration-700"
+						enterFrom="opacity-0 ease-out scale-25"
+						enterTo="opacity-100 ease-in scale-100"
+						leave="transition-all duration-1000"
+						leaveFrom="opacity-100 scale-100"
+						leaveTo="opacity-0 scale-75"
+					>
+						<ToastIcon toast={t} />
+						<p>{t.message?.toString()}</p>
+					</Transition>
+				);
+			}}
 		</Toaster>
 	);
 };
