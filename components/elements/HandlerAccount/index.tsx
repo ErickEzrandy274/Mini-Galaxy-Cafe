382a8efbfb47ac9efef3d88bccf8dba4b837fee1
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-03-28 13:12:56 +0700

    wrap a tag with Link tag from next

--- components/elements/HandlerAccount/index.tsx
+++ components/elements/HandlerAccount/index.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { toCapitalize } from "@utils";
 import { HandlerAccountProps } from "./interface";
+import Link from "next/link";
 
 const HandlerAccount: React.FC<HandlerAccountProps> = ({ content, href, className }) => {
 	
 	return (
 		<p className="my-6 mb-3 text-sm text-center text-gray-400">
 			{content}{" "}
-			<a
-				href={href}
-				className={className}
-			>
-				{toCapitalize(href.substring(1))} here
-			</a>
+			<Link href={href} passHref>
+				<a className={className}>{toCapitalize(href.substring(1))} here</a>
+			</Link>
 		</p>
 	);
 };
