7577fb0aafe1fe153fb0bf4dcb0e9605c42279e6
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-03-28 13:00:31 +0700

    create constant table header object

---
+++ components/elements/Table/constant.ts
@@ -0,0 +1,6 @@
+export const tableHeader = [
+	{ name: "Product name", headerDelay: 0.25, size: "all" },
+	{ name: "Category", headerDelay: 0.45, size: "desktop" },
+	{ name: "Amount", headerDelay: 0.65, size: "desktop" },
+	{ name: "Price", headerDelay: 0.85, size: "all" },
+];
