7454719f7d7bcab55bc372beb3beb14fd9295b4c
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2023-03-28 11:40:02 +0700

    fix styling priceinfo for mobile version

--- components/elements/PriceInfo/index.tsx
+++ components/elements/PriceInfo/index.tsx
@@ -1,10 +1,11 @@
 import React, { useMemo } from "react";
 import { motion } from "framer-motion";
-import { makeRupiahValue, menutitleAnimation } from "@utils";
+import { makeRupiahValue, menutitleAnimation, useWindowSize } from "@utils";
 import { PriceInfoProps } from "./interface";
 
 const PriceInfo: React.FC<PriceInfoProps> = ({ title, data, index }) => {
 	const { initial, animate, exit, transition } = menutitleAnimation;
+	const { width } = useWindowSize();
 	const subtotal: number = useMemo(() => {
 		return data?.reduce((acc, item) => {
 			return acc + item.price * item.amount;
@@ -24,9 +25,17 @@ const PriceInfo: React.FC<PriceInfoProps> = ({ title, data, index }) => {
 				...transition,
 				delay: 0.2 + 0.2 * (data.length + index),
 			}}
-			className={`flex justify-between w-[20rem]`}
+			className={`flex justify-between ${
+				width <= 350 ? "flex-col text-center" : "w-60 sm:w-[20rem]"
+			}`}
 		>
-			<p className="tracking-wide">{title}</p>
+			<p
+				className={`tracking-wide ${
+					width <= 350 ? "w-full" : "w-20"
+				} sm:w-full`}
+			>
+				{title}
+			</p>
 			<p className="tracking-widest">{makeRupiahValue(content)}</p>
 		</motion.section>
 	);
