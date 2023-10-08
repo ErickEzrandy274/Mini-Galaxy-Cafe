81d4b8e349d2ec83ebed1012e400b1123203e9cb
Erick Ezrandy <erick.ezrandy@ui.ac.id>
2023-10-08 15:45:19 +0700

    refactor: add placeholder and blur data url attribute

--- components/elements/OptimizedImage/index.tsx
+++ components/elements/OptimizedImage/index.tsx
@@ -1,5 +1,7 @@
+// reference for blur image: https://stackoverflow.com/a/69250346
 import React from "react";
 import { OptimizedImageProps } from "./interface";
+import { convertImage, toBase64 } from "@utils";
 import Image from "next/image";
 
 const OptimizedImage: React.FC<OptimizedImageProps> = ({
@@ -15,6 +17,10 @@ const OptimizedImage: React.FC<OptimizedImageProps> = ({
 			height={400}
 			objectFit="cover"
 			loading="lazy"
+			placeholder="blur"
+			blurDataURL={`data:image/svg+xml;base64,${toBase64(
+				convertImage(500, 400)
+			)}`}
 			className={welcoming ? "rounded-t-xl" : "rounded-lg"}
 		/>
 	);
